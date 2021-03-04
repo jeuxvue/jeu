cfbe01b0eff362d63da12664b6c789dd06bb3027
moltenship7750 <moltenship7750@gmail.com>
2021-03-04 10:51:02 +0700

    fix: increased timeout

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const instance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL as string,
-  timeout: 1000,
+  timeout: 2000,
   // headers: {
   //   'Access-Control-Allow-Origin': '*', // * или ваш домен
   //   'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE',
