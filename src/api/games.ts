245065c2d2fab70522b828c1d4d7a8a6e9d04401
moeta <teqqmu@gmail.com>
2021-05-06 00:18:33 +0700

    fix: hotfix

--- src/api/games.ts
+++ src/api/games.ts
@@ -2,7 +2,7 @@ import api from './axios'
 
 import { CategoryGames } from './types'
 
-export async function getGames(opts: { page: number }) {
+export async function getGames(opts?: { page: number }) {
   const params = { ordering: '-metacritic', ...opts }
   return api.get<CategoryGames>('games', { params }).then(r => r.data)
 }
